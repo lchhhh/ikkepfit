70f13d8256efb68acfd47d01233f9290c3c318ed
smithxmh <758479979@qq.com>
2019-04-10 23:28:40 +0800

    2328

---
+++ ikeepfit/ikeepfit/Models/Plan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+namespace ikeepfit.Models
+{
+    public class Plan
+    {
+    }
+    public class PlanDBContext : DbContext
+    {
+        public DbSet<Plan> Movies { get; set; }
+    }
+}
