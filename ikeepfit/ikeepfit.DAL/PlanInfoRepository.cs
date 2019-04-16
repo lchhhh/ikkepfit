1dcacb03df9286b838eea6bf54fe5d3a9d9d8623
lch <529896865@qq.com>
2019-04-16 17:18:29 +0800

    更新

---
+++ ikeepfit/ikeepfit.DAL/PlanInfoRepository.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ikeepfit.IDAL;
+using ikeepfit.Model;
+using System.Data.Entity;
+
+
+namespace ikeepfit.DAL
+{
+    public class PlanInfoRepository : IPlanInfoRepository
+    {
+        /// <summary>
+        /// 创建一个实体
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <returns></returns>
+        public int Create(mt_plan entity)
+        {
+            using (ikeepfitEntities db = new ikeepfitEntities())
+            {
+                db.Set<mt_plan>().Add(entity);
+                return db.SaveChanges();
+            }
+        }
+        /// <summary>
+        /// 删除一个实体
+        /// </summary>
+        /// <param name="id">主键ID</param>
+        /// <returns></returns>
+        public int Delete(int id)
+        {
+            using (ikeepfitEntities db = new ikeepfitEntities())
+            {
+                mt_plan entity = db.mt_plan.Find(id);
+                db.Set<mt_plan>().Remove(entity);
+                return db.SaveChanges();
+            }
+        }
+        /// <summary>
+        /// 修改一个实体
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <returns></returns>
+        public int Edit(mt_plan entity)
+        {
+            using (ikeepfitEntities db = new ikeepfitEntities())
+            {
+                db.Set<mt_plan>().Attach(entity);
+                db.Entry(entity).State = EntityState.Modified;
+                return db.SaveChanges();
+            }
+        }
+        /// <summary>
+        /// 获得一个实体
+        /// </summary>
+        /// <param name="id">主键ID</param>
+        /// <returns></returns>
+        public mt_plan GetById(int id)
+        {
+            using (ikeepfitEntities db = new ikeepfitEntities())
+            {
+                return db.mt_plan.Find(id);
+            }
+        }
+        /// <summary>
+        /// 获取列表
+        /// </summary>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        public IQueryable<mt_plan> GetList(ikeepfitEntities db)
+        {
+            IQueryable<mt_plan> list = db.mt_plan.AsQueryable();
+            return list;
+        }
+        /// <summary>
+        /// 判断一个实体是否存在
+        /// </summary>
+        /// <param name="id">主键ID</param>
+        /// <returns></returns>
+        public bool IsExist(int id)
+        {
+            using (ikeepfitEntities db = new ikeepfitEntities())
+            {
+                mt_plan entity = GetById(id);
+                if (entity != null)
+                    return true;
+                return false;
+            }
+        }
+    }
+}
---
+++ ikeepfit/ikeepfit.IBLL/IPlanInfoBLL.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ikeepfit.Model;
+
+namespace ikeepfit.IBLL
+{
+    public interface IPlanInfoBLL
+    {
+        /// <summary>
+        /// 获取列表
+        /// </summary>
+        /// <param name="pager">JQgrid分页</param>
+        /// <param name="queryStr">搜索条件</param>
+        /// <returns>列表</returns>
+        List<mt_plan> GetList();
+        /// <summary>
+        /// 创建一个实体
+        /// </summary>
+        /// <param name="errors">持久的错误信息</param>
+        /// <param name="model">模型</param>
+        /// <returns>是否成功</returns>
+        bool Create(mt_plan model);
+        /// <summary>
+        /// 删除一个实体
+        /// </summary>
+        /// <param name="errors">持久的错误信息</param>
+        /// <param name="id">id</param>
+        /// <returns>是否成功</returns>
+        bool Delete(int id);
+        /// <summary>
+        /// 修改一个实体
+        /// </summary>
+        /// <param name="errors">持久的错误信息</param>
+        /// <param name="model">模型</param>
+        /// <returns>是否成功</returns>
+        bool Edit(mt_plan model);
+        /// <summary>
+        /// 根据ID获得一个Model实体
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <returns>Model实体</returns>
+        mt_plan GetById(int id);
+        /// <summary>
+        /// 判断是否存在实体
+        /// </summary>
+        /// <param name="id">主键ID</param>
+        /// <returns>是否存在</returns>
+        bool IsExist(int id);
+    }
+}
