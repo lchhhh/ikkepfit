d961d32902b3a82f9269adbcf381d1e087a6eb40
lch <529896865@qq.com>
2019-04-19 15:43:44 +0800

    更新

---
+++ ikeepfit/ikeepfit.web/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ikeepfit.IServices;
+using ikeepfit.Services;
+using ikeepfit.Model;
+using ikeepfit.Repository;
+using ikeepfit.IRepository;
+using ikeepfit.WebCore;
+
+namespace ikeepfit.web.Controllers
+{
+    public class HomeController : BaseController
+    {
+        IPlanServices planservices;
+
+      
+        public HomeController(IPlanServices plans)
+        {
+            this.planservices = plans;
+        }
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult plan_menu()
+        {
+            ViewBag.controllername = RouteData.Values["controller"].ToString().ToLower();
+            ViewBag.planlist = planservices.QueryWhere(a => true).OrderByDescending(a => a.mt_plan_id);
+            return View();
+        }
+        public ActionResult plan_menu_add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult plan_menu_add(UserInfo userInfo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Content("error");
+            }
+            userInfo.RegTime = DateTime.Now;
+            demoEntities.UserInfo.Add(userInfo);
+            demoEntities.SaveChanges();
+            return Content("ok");
+        }
+        public ActionResult course_menu()
+        {
+            return View();
+        }
+      
+        public ActionResult book_menu()
+        {
+            return View();
+        }
+        public ActionResult knowledge_menu()
+        {
+            return View();
+        }
+        public ActionResult food_menu()
+        {
+            return View();
+        }
+
+
+        
+    }
+}
